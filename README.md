07069386526e0729a343701c5146661972e2af14
Mario Zupan <mario@zupzup.org>
2019-10-04 21:36:57 +0200

    setup instructions and init

---
+++ README.md
@@ -0,0 +1,19 @@
+# awaitedforsolong
+
+Base for an async/await coding dojo.
+
+## Setup
+
+This project uses currently unstable alpha versions of libraries such as futures and hyper, so we nee rust nightly to compile.
+
+Also, we need a rust version >= 1.39, you can check it by running.
+
+```bash
+rustc --version
+// rustc 1.40.0-nightly (488381ce9 2019-09-28)
+```
+
+Installing the nightly rust compiler is exaplained [here](https://doc.rust-lang.org/1.15.1/book/nightly-rust.html)
+
+
+Then, once that is installed, you should just be able to use `cargo run` and your IP should be displayed.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,79 @@
-fn main() {
-    println!("Hello, world!");
+extern crate tokio;
+extern crate hyper;
+
+use std::env;
+use std::io::{self, Write};
+use hyper_tls::HttpsConnector;
+use hyper::Client;
+use tokio::runtime::Runtime;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+fn main() -> Result<()> {
+    let rt = Runtime::new()?;
+    let url = match env::args().nth(1) {
+        Some(url) => url,
+        None => {
+            "https://api.ipify.org?format=json".to_owned()
+        }
+    };
+
+    let url = url.parse::<hyper::Uri>().unwrap();
+    rt.block_on(fetch_url(url))
 }
+
+async fn fetch_url(url: hyper::Uri) -> Result<()> {
+    let https = HttpsConnector::new().expect("https connector works");
+    let client = Client::builder().build::<_, hyper::Body>(https);
+
+    let res = client.get(url).await?;
+
+    println!("Response: {}", res.status());
+    println!("Headers: {:#?}\n", res.headers());
+
+    let mut body = res.into_body();
+
+    while let Some(next) = body.next().await {
+        let chunk = next?;
+        io::stdout().write_all(&chunk)?;
+    }
+
+    println!("\n\nDone!");
+
+    Ok(())
+}
+
+// #[tokio::main]
+// async fn main() -> Result<()> {
+//     let url = match env::args().nth(1) {
+//         Some(url) => url,
+//         None => {
+//             println!("Usage: client <url>");
+// 	    return Ok(());
+//         }
+//     };
+
+//     let url = url.parse::<hyper::Uri>().unwrap();
+//     fetch_url(url).await
+// }
+
+// async fn fetch_url(url: hyper::Uri) -> Result<()> {
+//     let https = HttpsConnector::new().expect("https connector works");
+//     let client = Client::builder().build::<_, hyper::Body>(https);
+
+//     let res = client.get(url).await?;
+
+//     println!("Response: {}", res.status());
+//     println!("Headers: {:#?}\n", res.headers());
+
+//     let mut body = res.into_body();
+
+//     while let Some(next) = body.next().await {
+//         let chunk = next?;
+//         io::stdout().write_all(&chunk)?;
+//     }
+
+//     println!("\n\nDone!");
+
+//     Ok(())
+// }
