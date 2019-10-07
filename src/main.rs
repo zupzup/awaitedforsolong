2b472083a9e5999de6b3f4b83f4b1e309e324537
Mario Zupan <mario@zupzup.org>
2019-10-07 18:28:12 +0200

    base impl

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-extern crate tokio;
-extern crate hyper;
-
-use std::env;
-use std::io::{self, Write};
 use hyper_tls::HttpsConnector;
 use hyper::Client;
 
@@ -10,32 +5,31 @@ type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let url = match env::args().nth(1) {
-        Some(url) => url,
-        None => "https://api.ipify.org?format=json".to_owned()
-    };
-
-    let url = url.parse::<hyper::Uri>().unwrap();
-    fetch_url(url).await
+    let url = "https://api.ipify.org?format=json".parse::<hyper::Uri>().unwrap();
+    let res = fetch_url(url).await?;
+    println!("{}", res);
+    Ok(())
 }
 
-async fn fetch_url(url: hyper::Uri) -> Result<()> {
+async fn fetch_url(url: hyper::Uri) -> Result<String> {
+    let mut result = String::new();
     let https = HttpsConnector::new().expect("https connector works");
     let client = Client::builder().build::<_, hyper::Body>(https);
 
     let res = client.get(url).await?;
 
-    println!("Response: {}", res.status());
-    println!("Headers: {:#?}\n", res.headers());
+    // println!("Response: {}", res.status());
+    // println!("Headers: {:#?}\n", res.headers());
 
     let mut body = res.into_body();
 
     while let Some(next) = body.next().await {
         let chunk = next?;
-        io::stdout().write_all(&chunk)?;
+        let bytes = String::from_utf8_lossy(&chunk);
+        result.push_str(&bytes);
     }
 
     println!("\n\nDone!");
 
-    Ok(())
+    Ok(result)
 }
