d18e75b408a74914182620f0b69da87294681710
Mario Zupan <mario@zupzup.org>
2019-10-07 17:36:44 +0200

    update deps

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,18 @@ use std::env;
 use std::io::{self, Write};
 use hyper_tls::HttpsConnector;
 use hyper::Client;
-use tokio::runtime::Runtime;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
-fn main() -> Result<()> {
-    let rt = Runtime::new()?;
+#[tokio::main]
+async fn main() -> Result<()> {
     let url = match env::args().nth(1) {
         Some(url) => url,
-        None => {
-            "https://api.ipify.org?format=json".to_owned()
-        }
+        None => "https://api.ipify.org?format=json".to_owned()
     };
 
     let url = url.parse::<hyper::Uri>().unwrap();
-    rt.block_on(fetch_url(url))
+    fetch_url(url).await
 }
 
 async fn fetch_url(url: hyper::Uri) -> Result<()> {
@@ -42,38 +39,3 @@ async fn fetch_url(url: hyper::Uri) -> Result<()> {
 
     Ok(())
 }
-
-// #[tokio::main]
-// async fn main() -> Result<()> {
-//     let url = match env::args().nth(1) {
-//         Some(url) => url,
-//         None => {
-//             println!("Usage: client <url>");
-// 	    return Ok(());
-//         }
-//     };
-
-//     let url = url.parse::<hyper::Uri>().unwrap();
-//     fetch_url(url).await
-// }
-
-// async fn fetch_url(url: hyper::Uri) -> Result<()> {
-//     let https = HttpsConnector::new().expect("https connector works");
-//     let client = Client::builder().build::<_, hyper::Body>(https);
-
-//     let res = client.get(url).await?;
-
-//     println!("Response: {}", res.status());
-//     println!("Headers: {:#?}\n", res.headers());
-
-//     let mut body = res.into_body();
-
-//     while let Some(next) = body.next().await {
-//         let chunk = next?;
-//         io::stdout().write_all(&chunk)?;
-//     }
-
-//     println!("\n\nDone!");
-
-//     Ok(())
-// }
